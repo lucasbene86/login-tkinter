9cf10ca5874cff1aaecc568a2bf42eb3a9166208
Joao Lucas <lucasbene86@gmail.com>
2021-04-15 05:13:00 -0300

    Adicionado o banco de dados. Criada a janela para o login.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Login')
+from tkinter import *
+
+janela = Tk()
+janela.title('Login')  # Titulo da janela.
+janela.geometry('500x600+400+50')  # Dimensão da janela e onde ela "Nasce".
+# janela.iconbitmap('icons/icon_janela/icone.ico')
+janela.resizable(0, 0)  # JANELA TRAVADA.
+# janela.state('zoomed')  # O programa já abre em tela cheia
+janela.config(background="#0085FF") # cor de fundo
+
+
+
+
+
+
+
+
+
+janela.mainloop()
