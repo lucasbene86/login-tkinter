c7d4963e2cef263fc2b24b1cb552b20f8bfc0b21
João Lucas <lucasbene86@gmail.com>
2021-04-25 18:54:45 -0300

    Adicionado o icone para janela.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import *
 janela = Tk()
 janela.title('Login')  # Titulo da janela.
 janela.geometry('350x450+400+50')  # Dimensão da janela e onde ela "Nasce".
-# janela.iconbitmap('icons/icon_janela/icone.ico')
+janela.iconbitmap('icons/icon_janela/login.ico')
 janela.resizable(0, 0)  # JANELA TRAVADA.
 # janela.state('zoomed')  # O programa já abre em tela cheia
 #janela.config(background="#000") # cor de fundo
